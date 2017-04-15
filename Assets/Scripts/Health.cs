cacc721a8241d3f0f5d09921d6c1c7be2b6dac24
Glicknork <glicknork@hotmail.com>
2017-04-15 15:41:58 +0100

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    PlayerController player;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (isServer)
+        {
+            currentHealth -= amount;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Debug.Log("Dead!");
+            }
+        }
+    }
+    void OnChangeHealth(int health)
+    {
+        player.FlashDamage();
+        Debug.Log("HEALTH = " + health);
+    }
+
+}
